3c26d9f507a8e3f29fc5e110fa3bb39b339a3fff
artem <artem.dos.25@gmail.com>
2024-12-02 13:50:33 +0200

    added field counter, methods run, swim , and countAnimals

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,9 @@
 public class Animal {
+    protected static int counter;
+    public void run(double meters){}
+    public void swim(double meters){}
 
+    public static void countAnimals(){
+        System.out.println("Всього тварин: " + counter);
+    }
 }
