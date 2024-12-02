d1f105ccab27c9e32ea00e684974af70c4a67fca
artem <artem.dos.25@gmail.com>
2024-12-02 13:52:26 +0200

    added constants, constructor, override methods to dog

--- src/Dog.java
+++ src/Dog.java
@@ -1,3 +1,31 @@
 public class Dog extends Animal{
 
+    public static final double METERS_RUN_LIM = 500;
+    public static final double METERS_SWIM_LIM = 10;
+
+    public Dog(double metersToRun, double metersToSwim){
+        ++counter;
+        run(metersToRun);
+        swim(metersToSwim);
+    }
+
+    @Override
+    public void run(double meters){
+        if(meters<METERS_RUN_LIM){
+            System.out.println("Пес пробіг " + meters + " метрів");
+        }
+        else{
+            System.out.println("Пес не може пробігти " + meters + " метрів");
+        }
+    }
+
+    @Override
+    public void swim(double meters){
+        if(meters<METERS_SWIM_LIM){
+            System.out.println("Пес проплив " + meters + " метрів");
+        }
+        else{
+            System.out.println("Пес не може проплисти " + meters + " метрів");
+        }
+    }
 }
