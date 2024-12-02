dbe3ac66f7831a05c174ac90c511d03c30d405a1
artem <artem.dos.25@gmail.com>
2024-12-02 14:01:44 +0200

    added constant, constructor and override methods to Cat

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,24 @@
 public class Cat extends Animal{
+    public static final double METERS_RUN_LIM = 200;
+
+    public Cat(double metersToRun){
+        ++counter;
+        run(metersToRun);
+        swim(0);
+    }
+
+    @Override
+    public void run(double meters){
+        if(meters<METERS_RUN_LIM){
+            System.out.println("Кіт пробіг " + meters + " метрів");
+        }
+        else{
+            System.out.println("Кіт не може пробігти " + meters + " метрів");
+        }
+    }
+
+    @Override
+    public void swim(double meters){
+        System.out.println("Кіт не вміє плавати");
+    }
 }
