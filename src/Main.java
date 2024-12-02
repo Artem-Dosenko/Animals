63a6a9c88d3cdc53ff72e89b0ec7b61da03499e6
artem <artem.dos.25@gmail.com>
2024-12-02 14:02:00 +0200

    added Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog bobik = new Dog(200, 5);
+        Cat Murzik = new Cat(100);
+        Dog Volt = new Dog(600, 6);
+        Cat Tom = new Cat(150);
+        Animal.countAnimals();
+    }
+}
